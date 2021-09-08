89edcdefead8de89f1fd573cbad493170b89f9f2
Roydano <roy.dano90@gmail.com>
2021-09-08 16:55:57 +0200

    creato colonne tabella

--- database/migrations/2021_09_08_141511_create_travels_table.php
+++ database/migrations/2021_09_08_141511_create_travels_table.php
@@ -15,7 +15,17 @@ class CreateTravelsTable extends Migration
     {
         Schema::create('travels', function (Blueprint $table) {
             $table->id();
-            
+            $table->string('location');
+            $table->text('travel_description');
+            $table->tinyInteger('days_travel')->unsigned();
+            $table->tinyInteger('person')->unsigned();
+            $table->string('overnight_stay', 50);
+            $table->string('hotel_name', 100);
+            $table->text('description')->nullable();
+            $table->tinyInteger('all_inclusive')->unsigned()->default(1);
+            $table->tinyInteger('parking_area')->unsigned()->default(1);
+            $table->tinyInteger('pool')->unsigned()->default(1);
+            $table->float('price', 7,2);
             $table->timestamps();
         });
     }
